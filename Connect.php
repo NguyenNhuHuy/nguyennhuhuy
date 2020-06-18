060809a7f6a7f47e27dc5b2c25b0064e93fa42d7
NguyenNhuHuy <67088321+NguyenNhuHuy@users.noreply.github.com>
2020-06-18 11:44:40 +0700

    Add files via upload

---
+++ Connect.php
@@ -0,0 +1,24 @@
+<?php
+$conn_string ="";
+$dbconn= pg_connect($conn_string);
+if (isset($_POST['username'])) {
+	# code...
+	$username = $_POST['username'];
+}
+
+if (isset($_POST['pass'])) {
+	# code...
+	$pass = $_POST['pass'];
+}
+$sql = "SELECT * FROM account WHERE _user='".$username."' AND _pass='".$pass."'";
+$result = pg_query($dbconn, $sql);
+$row = pg_num_rows($result);
+if ($row==1)
+{
+	echo "Login success";
+}
+else
+{
+	echo "Login failed";
+}
+?>
